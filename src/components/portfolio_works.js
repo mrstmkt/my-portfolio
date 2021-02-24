26010a0e8fb23856da04e27e9fb75634fce22f4a
MORISHITA Makoto <maktm0525@gmail.com>
2021-02-24 17:07:23 +0900

    works

--- src/components/portfolio_works.js
+++ src/components/portfolio_works.js
@@ -1,8 +1,31 @@
 import React from "react"
+import { Link, useStaticQuery, graphql } from "gatsby"
 import styles from "./portfolio.module.css"
 
 const WorksSection = ({ location, title, children }) => {
-  
+    const data = useStaticQuery(graphql`
+      query WorksQuery {
+        allMicrocmsWorks(sort: {order: ASC, fields: order}) {
+          edges {
+            node {
+              worksId
+              order
+              url
+              title
+              skill
+              description
+              image {
+                height
+                url
+                width
+              }
+            }
+          }
+        }
+      }    
+    `)
+    const works = data.allMicrocmsWorks.edges
+
     return (
         <section className={`${styles.workSection} ${styles.section}`} id="works-section">
         <div className={styles.inner}>
@@ -14,18 +37,34 @@ const WorksSection = ({ location, title, children }) => {
           </div>
           <div className={`${styles.sectionBody} ${styles.works}`}>
             <div className={`${styles.worksList} ${styles.jsMyGallery}`}>
-              <figure className={styles.worksItem}>
-                <a href="img/works/company.jpg" data-size="960x720" className="works-image-wrapper works-link hover-action">
-                  <img className={`${styles.worksImage} ${styles.lazyload} ${styles.fadein}`} src="img/placeholder4x3.png" data-src="img/works/company.jpg"
-                    alt="" />
-                </a>
-                <figcaption className={styles.worksBody}>
-                  <div className={styles.worksTitle}>サイト名が入る</div>
-                  <div className={styles.worksText}>Design / Coding(Responsive) / WordPress</div>
-                  <div className={styles.worksUrl}><a className="works-url-link" href="https://yahoo.co.jp" target="_blank"  rel="noreferrer">Link</a>
-                  </div>
-                </figcaption>
-              </figure>
+              {works.map(({node}) => {
+                  const title = node.title || node.articlesId
+                  let urlImage
+                  if(node.image.width === 1 && node.image.height === 1) {
+                    urlImage = (
+                      <div className={styles.worksAltImage}><div>{node.title}<br/>{node.description}</div></div>
+                    )
+                  }
+                  else {
+                    urlImage = (
+                      <img className={`${styles.worksImage} ${styles.lazyload} ${styles.fadein}`} src={`${node.image.url}?fit=crop&w=960&h=720`} data-src={node.image.url} alt="" />
+                    )
+                  }
+
+                  return (
+                    <figure className={styles.worksItem}>
+                    <a href={node.url}  className={`${styles.worksImageWrapper} ${styles.worksLink} ${styles.hoverAction}`} target="_blank"  rel="noreferrer">
+                      {urlImage}
+                    </a>
+                    <figcaption className={styles.worksBody}>
+                      <div className={styles.worksTitle}>{node.title}</div>
+                      <div className={styles.worksText}>{node.description}</div>
+                      <div className={styles.worksText}>{node.skill}</div>
+                    </figcaption>
+                  </figure>
+                  )
+                })}
+
             </div>
           </div>
         </div>

d5cd94e8be49d74552f1d710bedfecdc3a7b31ca
MORISHITA Makoto <maktm0525@gmail.com>
2021-03-04 17:34:13 +0900

    blog article

--- src/components/blog_article.js
+++ src/components/blog_article.js
@@ -3,20 +3,41 @@ import { Link } from "gatsby"
 import styles from "./blog.module.css"
 
 export const ArticleListItem = ({ article }) => {
-    return (
-        <article
+  let headerImage
+  if(article.headerImage.width === 1 && article.headerImage.height === 1) {
+    headerImage = (
+      <div>{article.title}</div>
+    )
+  }
+  else {
+    headerImage = (
+      <img src={`${article.headerImage.url}?fit=crop&w=960&h=720`} alt={article.title} />
+    )
+  }
+
+return (
+      <article
         className={styles.postListItem}
         itemScope
         itemType="http://schema.org/Article"
       >
-        <header>
-          <h2>
-            <Link to={`/blog/${article.articlesId}`} itemProp="url">
-              <span itemProp="headline">{article.title}</span>
-            </Link>
-          </h2>
-          <small>{article.date}</small>
-        </header>
+        <div className={styles.articleListItem}>
+          <div className={styles.articleListHeaderImage}>
+            {headerImage}
+          </div>
+          <div className={`${styles.articleListHeader} ${styles.blogPost}`}>
+            <header>
+              <h2>
+                <Link to={`/blog/${article.articlesId}`} itemProp="url">
+                  <span itemProp="headline">{article.title}</span>
+                </Link>
+              </h2>
+              <small>📅 {article.date}</small>
+            </header>
+
+          </div>
+
+        </div>
       </article>
     )
 }

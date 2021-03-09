0012e5964f4670b25bc672930ac957d01bc54413
MORISHITA Makoto <maktm0525@gmail.com>
2021-03-09 17:37:03 +0900

    nav

--- src/components/portfolio_layout.js
+++ src/components/portfolio_layout.js
@@ -48,17 +48,9 @@ const Layout = ({ location,  children }) => {
         </header>
         <main className={styles.content}>{children}</main>
         <footer className={styles.footer}>
-            {/* <div className={styles.pageTop} id="js-page-top">
-                トップへもどる
-            </div> */}
             <div className={styles.inner}>
                 <div className={styles.copyright}>Copyright&copy;{author}. All Rights Reserved.</div>
             </div>
-            {/* <div className={styles.inner}>
-              © {new Date().getFullYear()}, Built with
-              {` `}
-              <a href="https://www.gatsbyjs.com">Gatsby</a>
-            </div> */}
         </footer>
 
       </div>

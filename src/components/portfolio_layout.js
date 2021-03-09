b55f41e91e2e4ad0c20caf9724275fbf4641fa42
MORISHITA Makoto <maktm0525@gmail.com>
2021-03-09 17:19:50 +0900

    career

--- src/components/portfolio_layout.js
+++ src/components/portfolio_layout.js
@@ -48,9 +48,9 @@ const Layout = ({ location,  children }) => {
         </header>
         <main className={styles.content}>{children}</main>
         <footer className={styles.footer}>
-            <div className={styles.pageTop} id="js-page-top">
+            {/* <div className={styles.pageTop} id="js-page-top">
                 トップへもどる
-            </div>
+            </div> */}
             <div className={styles.inner}>
                 <div className={styles.copyright}>Copyright&copy;{author}. All Rights Reserved.</div>
             </div>
